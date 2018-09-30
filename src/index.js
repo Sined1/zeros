73a20b510a307d565a45cab5ed65609328f06620
Denis Babuskin <3169477@gmail.com>
2018-09-30 18:35:09 +0300

    add count zeros function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let count = 0;
+  for (let i = 5; (number/i) > 1; i = i*5) {
+    count = count + parseInt(number/i);
+  }
+  return count;
 }
